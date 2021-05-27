4450e9ec966aff3983023fbc330aca30e1b037c2
uzair <muhammaduzair1236@gmail.com>
2021-05-27 09:30:25 +0500

    changed settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,5 +144,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'chuzair1236@gmail.com'
-EMAIL_HOST_PASSWORD = 'Pak@Pak731'
+#EMAIL_HOST_USER = ''
+#EMAIL_HOST_PASSWORD = ''
